6e9131698eb62daee6ac4d74b55bb14fd3132643
Martin Minka <martin.minka@gmail.com>
2019-03-29 11:43:48 +0100

    add support for filter function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 const digest = require('file-digest');
 const readdir = require('recursive-readdir');
 
-module.exports = async dirpath => {
+module.exports = async (dirpath, filter) => {
   assert(typeof dirpath === 'string', 'directory path must be a string!');
 
   const hash = crypto.createHash('md5');
@@ -12,8 +12,10 @@ module.exports = async dirpath => {
   files = files.sort();
 
   for (const file of files) {
-    const fileHash = digest(file);
-    hash.update(fileHash);
+    if (!filter || filter(file)) {
+      const fileHash = digest(file);
+      hash.update(fileHash);
+    }
   }
 
   return hash.digest().toString('hex');
